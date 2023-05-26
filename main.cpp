32369358e888820c53ec89e0b4904a3e77b6f907
Michał Machnikowski <01178559@pw.edu.pl>
2023-05-26 22:29:17 +0200

    Simple Qt button

--- main.cpp
+++ main.cpp
@@ -1,18 +1,28 @@
 #include <QApplication>
 #include <QLabel>
 #include <QtCore>
+#include <QHBoxLayout>
+#include <QPushButton>
 #include <iostream>
 
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     std::cout << "Qt version: " << qVersion() << std::endl;
 
     QApplication app(argc, argv);
 
-    QLabel hello("Hello world!");
+    QWidget window{};
 
-    hello.show();
+    QLabel label{"Hello, world!"};
+    QPushButton button{"Click"};
+    QApplication::connect(&button, &QPushButton::released, [&label](){label.setText("changed");});
+
+    QHBoxLayout layout{};
+    layout.addWidget(&label);
+    layout.addWidget(&button);
+    window.setLayout(&layout);
+
+    window.show();
 
     return app.exec();
 }

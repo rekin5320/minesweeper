9628a6147ad7f96b672895f929a8aa5606a95742
Michał Machnikowski <01178559@pw.edu.pl>
2023-06-03 02:41:28 +0200

    Basic buttons grid

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <QtWidgets/QPushButton>
 
 #include "minesweeper_ui.hpp"
+#include "logic.hpp"
+
+
+class GameUI: public Ui::MainWindow {
+public:
+    void create_tiles(Board& board) {
+        for (auto& tile : board.Tiles) {
+            auto *button = new QPushButton();
+            QApplication::connect(button, &QPushButton::released, [this](){this->label->setText("changed");});
+            gridLayout->addWidget(button, tile.y, tile.x);
+        }
+    };
+};
 
 
 int main(int argc, char **argv) {
@@ -8,10 +22,12 @@ int main(int argc, char **argv) {
 
     QApplication app(argc, argv);
     QMainWindow window;
-    Ui::MainWindow ui;
+    GameUI ui;
     ui.setupUi(&window);
+    Board board{15, 10};
+    ui.create_tiles(board);
 
-    QApplication::connect(ui.button, &QPushButton::released, [&ui](){ui.label->setText("changed");});
+    // QApplication::connect(ui.button, &QPushButton::released, [&ui](){ui.label->setText("changed");});
 
     window.show();
     return app.exec();

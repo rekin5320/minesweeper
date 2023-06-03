75b2987ecdba6804bb2bd6b9f7cf24d9893bf787
Jakub Pęk <01178575@pw.edu.pl>
2023-06-03 10:27:39 +0200

    Added tests for Game class

--- tests/test_logic.cpp
+++ tests/test_logic.cpp
@@ -3,8 +3,8 @@
 #include "catch.hpp"
 #include "../logic.hpp"
 
-
-TEST_CASE("Tile") {
+TEST_CASE("Tile")
+{
     Tile tile(1, 3);
     REQUIRE(tile.x == 1);
     REQUIRE(tile.y == 3);
@@ -13,13 +13,15 @@ TEST_CASE("Tile") {
     REQUIRE(!tile.is_flagged);
     REQUIRE(tile.num_adjacent_bombs == 0);
 
-    SECTION("uncover()") {
+    SECTION("uncover()")
+    {
         tile.uncover();
         REQUIRE(!tile.is_covered);
         REQUIRE_THROWS_AS(tile.uncover(), std::invalid_argument);
     }
 
-    SECTION("flag() & unflag()") {
+    SECTION("flag() & unflag()")
+    {
         REQUIRE_THROWS_AS(tile.unflag(), std::invalid_argument);
         tile.flag();
         REQUIRE(tile.is_flagged);
@@ -30,15 +32,19 @@ TEST_CASE("Tile") {
     }
 }
 
-TEST_CASE("Board") {
-    SECTION("3×2") {
+TEST_CASE("Board")
+{
+    SECTION("3×2")
+    {
         Board board{3, 2};
 
         REQUIRE(board.WIDTH == 3);
         REQUIRE(board.HEIGHT == 2);
 
-        for (unsigned int x = 0; x < 3; x++) {
-            for (unsigned int y = 0; y < 2; y++) {
+        for (unsigned int x = 0; x < 3; x++)
+        {
+            for (unsigned int y = 0; y < 2; y++)
+            {
                 Tile &tile = board.get_tile(x, y);
                 REQUIRE(tile.x == x);
                 REQUIRE(tile.y == y);
@@ -49,29 +55,37 @@ TEST_CASE("Board") {
             }
         }
 
-        SECTION("get_tile") {
-            SECTION("valid coordinates") {
+        SECTION("get_tile")
+        {
+            SECTION("valid coordinates")
+            {
                 board.get_tile(1, 1).is_bomb = true;
                 REQUIRE(board.get_tile(1, 1).is_bomb);
             }
 
-            SECTION("invalid coordinates") {
+            SECTION("invalid coordinates")
+            {
                 REQUIRE_THROWS_AS(board.get_tile(10, 20), std::invalid_argument);
                 REQUIRE_THROWS_AS(board.get_tile(1, 2), std::invalid_argument);
                 REQUIRE_THROWS_AS(board.get_tile(3, 1), std::invalid_argument);
             }
         }
 
-        SECTION("random_tile") {
+        SECTION("random_tile")
+        {
             REQUIRE(!board.random_tile().is_bomb);
         }
 
-        SECTION("generate_bombs") {
+        SECTION("generate_bombs")
+        {
             board.generate_bombs(4);
             unsigned int count = 0;
-            for (unsigned int x = 0; x < 3; x++) {
-                for (unsigned int y = 0; y < 2; y++) {
-                    if (board.get_tile(x, y).is_bomb) {
+            for (unsigned int x = 0; x < 3; x++)
+            {
+                for (unsigned int y = 0; y < 2; y++)
+                {
+                    if (board.get_tile(x, y).is_bomb)
+                    {
                         count++;
                     }
                 }
@@ -79,7 +93,8 @@ TEST_CASE("Board") {
             REQUIRE(count == 4);
         }
 
-        SECTION("count_adjacent_bombs") {
+        SECTION("count_adjacent_bombs")
+        {
             board.random.seed(37);
             board.generate_bombs(2);
             board.count_adjacent_bombs();
@@ -97,13 +112,15 @@ TEST_CASE("Board") {
         }
     }
 
-    SECTION("5×4") {
+    SECTION("5×4")
+    {
         Board board{5, 4};
 
         REQUIRE(board.WIDTH == 5);
         REQUIRE(board.HEIGHT == 4);
 
-        SECTION("count_adjacent_bombs") {
+        SECTION("count_adjacent_bombs")
+        {
             board.random.seed(21);
             board.generate_bombs(7);
             board.count_adjacent_bombs();
@@ -138,5 +155,110 @@ TEST_CASE("Board") {
             REQUIRE(board.get_tile(3, 3).is_bomb);
             REQUIRE(board.get_tile(4, 3).is_bomb);
         }
+    }
+}
+
+TEST_CASE("Game")
+{
+    SECTION("BEGINNER")
+    {
+        Game game = Game(Difficulty::BEGINNER);
+
+        REQUIRE(game.board.WIDTH == 9);
+        REQUIRE(game.board.HEIGHT == 9);
+        REQUIRE(game.num_bombs == 10);
+    }
+
+    SECTION("INTERMEDIATE")
+    {
+        Game game = Game(Difficulty::INTERMEDIATE);
+
+        REQUIRE(game.board.WIDTH == 16);
+        REQUIRE(game.board.HEIGHT == 16);
+        REQUIRE(game.num_bombs == 40);
+    }
+
+    SECTION("EXPERT")
+    {
+        Game game = Game(Difficulty::EXPERT);
+
+        REQUIRE(game.board.WIDTH == 30);
+        REQUIRE(game.board.HEIGHT == 16);
+        REQUIRE(game.num_bombs == 99);
+    }
+
+    SECTION("CUSTOM")
+    {
+        Game game = Game(Difficulty::CUSTOM, 5, 4, 7);
+
+        REQUIRE(game.board.WIDTH == 5);
+        REQUIRE(game.board.HEIGHT == 4);
+        REQUIRE(game.num_bombs == 7);
+    }
+
+    SECTION("CUSTOM_MISSING_ARGS")
+    {
+        REQUIRE_THROWS_AS(Game(Difficulty::CUSTOM, 5, 4), std::invalid_argument);
+        REQUIRE_THROWS_AS(Game(Difficulty::CUSTOM), std::invalid_argument);
+    }
+
+    SECTION("start()")
+    {
+        Game game = Game(Difficulty::INTERMEDIATE);
+        game.start();
+
+        unsigned int count = 0;
+        for (unsigned int x = 0; x < game.board.HEIGHT; x++)
+        {
+            for (unsigned int y = 0; y < game.board.WIDTH; y++)
+            {
+                if (game.board.get_tile(x, y).is_bomb)
+                {
+                    count++;
+                }
+            }
+        }
+        REQUIRE(count == 40);
+    }
+
+    SECTION("is_game_over()")
+    {
+        Game game = Game(Difficulty::INTERMEDIATE);
+        game.start();
+
+        REQUIRE_FALSE(game.is_game_over());
+
+        for (unsigned int x = 0; x < game.board.HEIGHT; x++)
+        {
+            for (unsigned int y = 0; y < game.board.WIDTH; y++)
+            {
+                if (game.board.get_tile(x, y).is_bomb)
+                {
+                    game.board.get_tile(x, y).uncover();
+                    break;
+                }
+            }
+        }
+        REQUIRE(game.is_game_over());
+    }
+
+    SECTION("is_game_won()")
+    {
+        Game game = Game(Difficulty::INTERMEDIATE);
+        game.start();
+
+        REQUIRE_FALSE(game.is_game_won());
+
+        for (unsigned int x = 0; x < game.board.HEIGHT; x++)
+        {
+            for (unsigned int y = 0; y < game.board.WIDTH; y++)
+            {
+                if (!game.board.get_tile(x, y).is_bomb)
+                {
+                    game.board.get_tile(x, y).uncover();
+                }
+            }
+        }
+        REQUIRE(game.is_game_won());
     }
 }

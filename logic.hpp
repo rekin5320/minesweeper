fe251c381b6f42594361d9c7a457a5f727e312b8
Michał Machnikowski <01178559@pw.edu.pl>
2023-06-04 22:32:42 +0200

    Changed emoji used after game over

--- logic.hpp
+++ logic.hpp
@@ -452,7 +452,7 @@ public:
                 std::cout << "Game over! You lost the game in: " << get_formatted_elapsed_time() << "!\n";
                 if (with_gui)
                 {
-                    ui.mainbutton->setText("🤕");
+                    ui.mainbutton->setText("🙁");
                 }
             }
             else if (is_game_won())

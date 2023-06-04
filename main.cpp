6e18004db5768ab28cdac661feb278690e9cc821
Jakub Pęk <01178575@pw.edu.pl>
2023-06-04 14:27:18 +0200

    Formating

--- main.cpp
+++ main.cpp
@@ -2,28 +2,31 @@
 #include "logic.hpp"
 #include "minesweeper_ui.hpp"
 
-
-class GameUI: public Ui::MainWindow {
+class GameUI : public Ui::MainWindow
+{
 public:
-    Game& game;
+    Game &game;
 
-    GameUI(Game& game): game(game) {};
+    GameUI(Game &game) : game(game){};
 
-    void create_tiles(Board& board) {
-        for (auto& tile : board.Tiles) {
+    void create_tiles(Board &board)
+    {
+        for (auto &tile : board.Tiles)
+        {
             unsigned int x = tile.x, y = tile.y;
             tile.create_button();
             gridLayout->addWidget(tile.button.get(), y, x);
             tile.button->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Minimum);
-            QObject::connect(tile.button.get(), &MyButton::leftButtonClicked, [&board, x, y](){board.uncover_tile(x, y);});
-            QObject::connect(tile.button.get(), &MyButton::rightButtonClicked, [&board, x, y](){
-                board.flag_or_unflag_tile(x, y);});
+            QObject::connect(tile.button.get(), &MyButton::leftButtonClicked, [&board, x, y]()
+                             { board.uncover_tile(x, y); });
+            QObject::connect(tile.button.get(), &MyButton::rightButtonClicked, [&board, x, y]()
+                             { board.flag_or_unflag_tile(x, y); });
         }
     };
 };
 
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     std::cout << "Qt version: " << qVersion() << std::endl;
 
     QApplication app(argc, argv);

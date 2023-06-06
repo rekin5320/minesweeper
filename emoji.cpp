4b5ee0a29ebb4ce8cb353611d038d6242e0f29f0
Jakub Pęk <01178575@pw.edu.pl>
2023-06-06 20:25:38 +0200

    Added files with emoji

---
+++ emoji.cpp
@@ -0,0 +1,11 @@
+#include "emoji.h"
+
+namespace Emoji
+{
+    const std::string SmileyFace = "😊";
+    const std::string SadFace = "😢";
+    const std::string CoolFace = "😎";
+    const std::string Flag = "🚩";
+    const std::string Pickaxe = "⛏️";
+
+}
---
+++ emoji.h
@@ -0,0 +1,15 @@
+#ifndef EMOJI_H
+#define EMOJI_H
+
+#include <string>
+
+namespace Emoji
+{
+    extern const std::string SmileyFace;
+    extern const std::string SadFace;
+    extern const std::string CoolFace;
+    extern const std::string Flag;
+    extern const std::string Pickaxe;
+}
+
+#endif // EMOJI_H

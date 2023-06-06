c30c6272917b8940604326d4937533439657cdc1
Jakub Pęk <01178575@pw.edu.pl>
2023-06-06 14:50:26 +0200

    Corected tests to use getters

--- tests/test_game.cpp
+++ tests/test_game.cpp
@@ -9,9 +9,9 @@ TEST_CASE("Game")
         Game game;
         game.set_level(Difficulty::BEGINNER);
 
-        REQUIRE(game.board.width == 9);
-        REQUIRE(game.board.height == 9);
-        REQUIRE(game.num_bombs == 10);
+        REQUIRE(game.get_board().width == 9);
+        REQUIRE(game.get_board().height == 9);
+        REQUIRE(game.get_num_bombs() == 10);
     }
 
     SECTION("INTERMEDIATE")
@@ -19,9 +19,9 @@ TEST_CASE("Game")
         Game game;
         game.set_level(Difficulty::INTERMEDIATE);
 
-        REQUIRE(game.board.width == 16);
-        REQUIRE(game.board.height == 16);
-        REQUIRE(game.num_bombs == 40);
+        REQUIRE(game.get_board().width == 16);
+        REQUIRE(game.get_board().height == 16);
+        REQUIRE(game.get_num_bombs() == 40);
     }
 
     SECTION("EXPERT")
@@ -29,9 +29,9 @@ TEST_CASE("Game")
         Game game;
         game.set_level(Difficulty::EXPERT);
 
-        REQUIRE(game.board.width == 30);
-        REQUIRE(game.board.height == 16);
-        REQUIRE(game.num_bombs == 99);
+        REQUIRE(game.get_board().width == 30);
+        REQUIRE(game.get_board().height == 16);
+        REQUIRE(game.get_num_bombs() == 99);
     }
 
     SECTION("CUSTOM")
@@ -39,9 +39,9 @@ TEST_CASE("Game")
         Game game;
         game.set_level(Difficulty::CUSTOM, 5, 4, 7);
 
-        REQUIRE(game.board.width == 5);
-        REQUIRE(game.board.height == 4);
-        REQUIRE(game.num_bombs == 7);
+        REQUIRE(game.get_board().width == 5);
+        REQUIRE(game.get_board().height == 4);
+        REQUIRE(game.get_num_bombs() == 7);
     }
 
     SECTION("CUSTOM_MISSING_ARGS")
@@ -57,17 +57,17 @@ TEST_CASE("Game")
         Game game;
         game.set_level(Difficulty::INTERMEDIATE);
 
-        REQUIRE(!game.has_started);
+        REQUIRE(!game.get_has_started());
         game.start();
 
-        REQUIRE(game.has_started);
-        REQUIRE(!game.has_ended);
-        REQUIRE(game.first_click);
+        REQUIRE(game.get_has_started());
+        REQUIRE(!game.get_has_ended());
+        REQUIRE(game.get_first_click());
 
         unsigned int count = 0;
-        for (unsigned int x = 0; x < game.board.height; x++) {
-            for (unsigned int y = 0; y < game.board.width; y++) {
-                if (game.board.get_tile(x, y).is_bomb) {
+        for (unsigned int x = 0; x < game.get_board().height; x++) {
+            for (unsigned int y = 0; y < game.get_board().width; y++) {
+                if (game.get_board().get_tile(x, y).is_bomb) {
                     count++;
                 }
             }
@@ -84,10 +84,10 @@ TEST_CASE("Game")
         REQUIRE(game.left_bombs() == 40);
 
         int count = 0;
-        for (unsigned int x = 0; x < game.board.height; x++) {
-            for (unsigned int y = 0; y < game.board.width; y++) {
-                if (game.board.get_tile(x, y).is_bomb) {
-                    game.board.get_tile(x, y).flag();
+        for (unsigned int x = 0; x < game.get_board().height; x++) {
+            for (unsigned int y = 0; y < game.get_board().width; y++) {
+                if (game.get_board().get_tile(x, y).is_bomb) {
+                    game.get_board().get_tile(x, y).flag();
                     REQUIRE(game.left_bombs() == 40 - ++count);
                 }
             }
@@ -95,10 +95,10 @@ TEST_CASE("Game")
 
         REQUIRE(game.left_bombs() == 0);
 
-        for (unsigned int x = 0; x < game.board.height; x++) {
-            for (unsigned int y = 0; y < game.board.width; y++) {
-                if (game.board.get_tile(x, y).is_flagged) {
-                    game.board.get_tile(x, y).unflag();
+        for (unsigned int x = 0; x < game.get_board().height; x++) {
+            for (unsigned int y = 0; y < game.get_board().width; y++) {
+                if (game.get_board().get_tile(x, y).is_flagged) {
+                    game.get_board().get_tile(x, y).unflag();
                     REQUIRE(game.left_bombs() == 40 - --count);
                 }
             }
@@ -111,30 +111,30 @@ TEST_CASE("Game")
         game.set_level(Difficulty::INTERMEDIATE);
         game.start();
 
-        for (unsigned int x = 0; x < game.board.height; x++) {
-            for (unsigned int y = 0; y < game.board.width; y++) {
-                if (game.board.get_tile(x, y).is_bomb) {
-                    REQUIRE(game.board.get_tile(x, y).is_covered);
+        for (unsigned int x = 0; x < game.get_board().height; x++) {
+            for (unsigned int y = 0; y < game.get_board().width; y++) {
+                if (game.get_board().get_tile(x, y).is_bomb) {
+                    REQUIRE(game.get_board().get_tile(x, y).is_covered);
                 }
             }
         }
 
         REQUIRE_FALSE(game.is_game_over());
 
-        for (unsigned int x = 0; x < game.board.height; x++) {
-            for (unsigned int y = 0; y < game.board.width; y++) {
-                if (game.board.get_tile(x, y).is_bomb) {
-                    game.board.get_tile(x, y).uncover();
+        for (unsigned int x = 0; x < game.get_board().height; x++) {
+            for (unsigned int y = 0; y < game.get_board().width; y++) {
+                if (game.get_board().get_tile(x, y).is_bomb) {
+                    game.get_board().get_tile(x, y).uncover();
                     break;
                 }
             }
         }
         REQUIRE(game.is_game_over());
 
-        for (unsigned int x = 0; x < game.board.height; x++) {
-            for (unsigned int y = 0; y < game.board.width; y++) {
-                if (game.board.get_tile(x, y).is_bomb) {
-                    REQUIRE_FALSE(game.board.get_tile(x, y).is_covered);
+        for (unsigned int x = 0; x < game.get_board().height; x++) {
+            for (unsigned int y = 0; y < game.get_board().width; y++) {
+                if (game.get_board().get_tile(x, y).is_bomb) {
+                    REQUIRE_FALSE(game.get_board().get_tile(x, y).is_covered);
                 }
             }
         }
@@ -148,10 +148,10 @@ TEST_CASE("Game")
 
         REQUIRE_FALSE(game.is_game_won());
 
-        for (unsigned int x = 0; x < game.board.height; x++) {
-            for (unsigned int y = 0; y < game.board.width; y++) {
-                if (!game.board.get_tile(x, y).is_bomb) {
-                    game.board.get_tile(x, y).uncover();
+        for (unsigned int x = 0; x < game.get_board().height; x++) {
+            for (unsigned int y = 0; y < game.get_board().width; y++) {
+                if (!game.get_board().get_tile(x, y).is_bomb) {
+                    game.get_board().get_tile(x, y).uncover();
                 }
             }
         }
@@ -164,30 +164,30 @@ TEST_CASE("Game")
         game.set_level(Difficulty::INTERMEDIATE);
         game.start();
 
-        game.board.get_tile(0, 0).flag();
+        game.get_board().get_tile(0, 0).flag();
 
-        for (unsigned int x = 0; x < game.board.height; x++) {
-            for (unsigned int y = 0; y < game.board.width; y++) {
+        for (unsigned int x = 0; x < game.get_board().height; x++) {
+            for (unsigned int y = 0; y < game.get_board().width; y++) {
                 game.uncover_tile(x, y);
             }
         }
 
-        REQUIRE(game.has_started);
+        REQUIRE(game.get_has_started());
         REQUIRE(game.is_game_over());
-        REQUIRE(game.has_ended);
-        REQUIRE_FALSE(game.first_click);
+        REQUIRE(game.get_has_ended());
+        REQUIRE_FALSE(game.get_first_click());
 
         game.play_again();
-        REQUIRE(game.has_started);
+        REQUIRE(game.get_has_started());
 
-        for (unsigned int x = 0; x < game.board.height; x++) {
-            for (unsigned int y = 0; y < game.board.width; y++) {
-                REQUIRE(game.board.get_tile(x, y).is_covered);
-                REQUIRE(!game.board.get_tile(x, y).is_flagged);
+        for (unsigned int x = 0; x < game.get_board().height; x++) {
+            for (unsigned int y = 0; y < game.get_board().width; y++) {
+                REQUIRE(game.get_board().get_tile(x, y).is_covered);
+                REQUIRE(!game.get_board().get_tile(x, y).is_flagged);
             }
         }
 
-        REQUIRE_FALSE(game.has_ended);
-        REQUIRE(game.first_click);
+        REQUIRE_FALSE(game.get_has_ended());
+        REQUIRE(game.get_first_click());
     }
 }
